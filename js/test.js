57117320642705094412c50a9159911fcc31a024
Eduard-Konovka <ed098ua@gmail.com>
2021-09-28 23:54:56 +0300

    Фавикон

--- js/test.js
+++ js/test.js
@@ -1,11 +1,8 @@
-function sum(num) {
-  if (num === 0) {
-    return 0
-  } else {
-    let res = num + sum(--num)
-    console.log(res)
-    return res
-  }
+const getUsers = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users = response.json()
+
+  return users
 }
 
-sum(2) //10
+getUsers().then(users => console.log(users))
