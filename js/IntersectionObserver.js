75d19c6995e6f50436b6eec05b85acf693482ce1
Eduard Konovka <ed098ua@gmail.com>
2023-02-12 08:07:32 +0200

    Adds comments in IntersectionObserver.js

--- js/IntersectionObserver.js
+++ js/IntersectionObserver.js
@@ -9,26 +9,26 @@ const options = {
 const callback = (elements, observer) => {
   elements.forEach(element => {
     if (element.isIntersecting) {
-      console.log('element', element)
+      console.log('element', element) // Здесь логика того, что нужно делать, когда цель попадёт в поле видимости
     }
   })
 }
 
 const observer = new IntersectionObserver(callback, options)
 
-var target = document.querySelector('.target')
+var target = document.querySelector('.target') // Это то, что мы отслеживаем
 observer.observe(target)
 
 // --- сокращённая запись ---
 
-// const callback = (elements, observer) => {
-//   elements.forEach(element => {
-//     if (element.isIntersecting) {
-//       console.log('element', element)
-//     }
-//   })
-// }
+const callback = (elements, observer) => {
+  elements.forEach(element => {
+    if (element.isIntersecting) {
+      console.log('element', element) // Здесь логика того, что нужно делать, когда цель попадёт в поле видимости
+    }
+  })
+}
 
-// const observer = new IntersectionObserver(callback)
+const observer = new IntersectionObserver(callback)
 
-// observer.observe(document.querySelector('.target'))
+observer.observe(document.querySelector('.target')) // Это то, что мы отслеживаем
